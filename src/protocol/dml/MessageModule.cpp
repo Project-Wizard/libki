28478fd80cdf2b382a38859bdd43d829599121af
Rocket Programmer <rocketgamer580@gmail.com>
2022-04-02 18:52:08 -0500

    Revert "message-module: Try this"
    This reverts commit 61d891bd3a147a624449cd6ff5c85bac5c82c9d4.

--- src/protocol/dml/MessageModule.cpp
+++ src/protocol/dml/MessageModule.cpp
@@ -117,7 +117,7 @@ namespace dml
 
 	void MessageModule::sort_lookup()
 	{
-		uint8_t message_type = 0;
+		uint8_t message_type = 1;
 
 		// First, clear the message type map since we're going to be
 		// moving everything around
